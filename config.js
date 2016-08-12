57919622522aa42035b597847b06ffcc1954ceeb
Eric <to.erictran@gmail.com>
2016-08-11 21:20:39 -0700

    build complete

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	movie_root_url: 'https://api.themoviedb.org/3/movie',
+	movie_root_url: 'https://api.themoviedb.org/3',
 	movie_api_key: '6cb8d1dbda0f4077648f28b2c46a20cd',
 	yt_api_key: 'AIzaSyDLI1PT8oiax7uQpacna-jYXdsMN7SX9p0',
 	yt_root_url: 'https://www.googleapis.com/youtube/v3/search'
